9418351855105a36563f6f797c168300a04fc431
Jared Elliott <bootcamp@jaredclt.com>
2024-01-31 21:10:24 -0500

    Updated model relationships.

--- model/index.js
+++ model/index.js
@@ -1,6 +1,25 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
 
 module.exports = { User, Post, Comment };

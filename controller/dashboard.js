ce898474a6c006c238c642b55ffd6ad6330f526a
Jared Elliott <bootcamp@jaredclt.com>
2024-01-31 23:06:27 -0500

    Updated routes.

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const { User, Comment, Post } = require('../model');
 
 router.use('/dashboard', (req, res) => {
+    // req.session.user_id
 
-    
     res.render('dashboard', {layout : 'main'})
   })
 
--- controller/index.js
+++ controller/index.js
@@ -1,24 +1,34 @@
 const router = require('express').Router();
 const { User, Comment, Post } = require('../model');
 const dashboardRoute = require('./dashboard');
+const authRoute = require('./auth');
 //const apiRoutes = require('./api');
 //router.use('/api', apiRoutes)
 
 router.use('/dashboard', dashboardRoute)
+router.use('/login', authRoute)
+router.use('/signup', authRoute)
 
 //Homepage routing
 router.use('/', async (req, res) => {
 
   const dbPostData = await Post.findAll({
-    include:
-      {
+    order: [[ 'created_on', 'DESC']],
+    include: [{
         model: User,
-        attributes: ['name']
+        attributes: ['name'],
+        required: true
+      },
+      {
+        model: Comment,
+        required: false,
+        attributes: ['id'],
       }
+    ],
   })
 
   const postData = dbPostData.map((data) => data.get({ plain: true }));
-  console.log(postData);
+  console.log(postData)
 
   res.render('homepage', { postData , layout : 'main' })
 });
